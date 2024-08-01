ed819ac4aefdcb8476c1be684579ccc04b1a554b
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-02 02:29:35 +0300

    mixed type variables

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,6 +7,6 @@ The second element is the square of the int/float v and annotated as a float'''
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     ''' a function that takes mixed arg and returns a tuple'''
     return (k, v * v)
