b1c058f34be30cd1afdd473bf2cd272ef7e36fe5
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-02 03:07:45 +0300

    mixing fun

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,5 +3,10 @@
 
 from typing import Iterable, Sequence, List, Tuple
 
-def element_length(lst:Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''a function that takes a list of a sequence: tuple or list
+    Returns: A list of tuples, each contains a sequence
+    from the input list and its length.
+    '''
     return [(i, len(i)) for i in lst]
