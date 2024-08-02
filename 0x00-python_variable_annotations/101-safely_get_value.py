f27ed0bbeda85b041aa45e3eae7db4f01e9b5ada
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-02 03:40:10 +0300

    TypeVar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,7 +4,9 @@
 from typing import Mapping, Any, Union, TypeVar
 T = TypeVar('T')
 
+
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None):
+    '''TypeVar utilized'''
     if key in dct:
         return dct[key]
     else:
