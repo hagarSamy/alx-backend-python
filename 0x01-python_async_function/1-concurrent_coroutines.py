083bc41c7dfe523dcee25dd1ea9b24ebc24dcd47
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-12 01:03:05 +0300

    tasks as completed

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+synchronous coroutine that takes in an integer argument
+(max_delay, with a default value of 10) named wait_random
+that waits for a random delay between 0 and max_delay
+seconds and eventually returns it.
+"""
+
+
+async def wait_n(n: int, max_delay: int = 10) -> float:
+    """Async function that returns a random number after waiting """
+    wait_random = __import__('0-basic_async_syntax').wait_random
+    import asyncio
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    results = []
+
+    for completed_task in asyncio.as_completed(tasks):
+        result = await completed_task
+        results.append(result)
+
+    return results
