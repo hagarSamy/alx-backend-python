7affdae8cc3f9ced98a266a27669ea44e4dc9ebc
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-12 01:04:47 +0300

    tasks as completed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,7 @@ async def wait_n(n: int, max_delay: int = 10) -> float:
     wait_random = __import__('0-basic_async_syntax').wait_random
     import asyncio
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    results = []
+    results: float = []
 
     for completed_task in asyncio.as_completed(tasks):
         result = await completed_task
