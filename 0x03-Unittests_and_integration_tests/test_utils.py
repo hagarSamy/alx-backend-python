28110062149cb67037ac052f734e5c3c78cb4067
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-31 13:49:54 +0300

    testing errors

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -11,11 +11,16 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({}, ("a",), None),
+        ({"a": 1}, ("a", "b"), None),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         '''Test access to nested map using parameterized inputs.'''
-        self.assertEqual(access_nested_map(nested_map, path), expected)
-
+        if expected is None:
+            with self.assertRaises(KeyError):
+                access_nested_map(nested_map, path)
+        else:
+            self.assertEqual(access_nested_map(nested_map, path), expected)
 
 if __name__ == '__main__':
     unittest.main()
