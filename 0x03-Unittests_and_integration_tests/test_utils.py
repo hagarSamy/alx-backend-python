e9b7ce673bfb75e49336bfcc680af58590981571
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-31 14:51:43 +0300

    mock.patch modified

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -38,7 +38,7 @@ class TestGetJson(unittest.TestCase):
     @patch('requests.get')
     def test_get_json(self, test_url, test_payload, mock_get):
         # Set up the mock response object
-        mock_get.return_value.json.return_value = {test_url: test_payload}
+        mock_get.return_value.json.return_value = test_payload
         
         # Call the function under test
         result = get_json(test_url)
